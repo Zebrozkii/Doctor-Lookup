1a43215b7b01d6d7214cc7de0a58f057c8076598
Jimmy Zebroski <zebrozkii@gmail.com>
2019-04-07 15:01:44 -0700

    ok i got the name working will output a doctors name address and phonenumber

--- src/doctor.js
+++ src/doctor.js
@@ -13,12 +13,22 @@ $(document).ready(function() {
     console.log(doctorSearch);
     let promise = doctorSearch.getName(searchName);
     console.log(promise);
+    $('#doctorInfo').empty();
+
     promise.then(function(response){
-      $('#doctor-info').empty();
       let body = JSON.parse(response);
-      var info = $('#doctor-info');
+      var info = body.data;
       console.log(info);
-      
+      if(info.length === 0){
+        $('#doctorInfo').text("there are no doctors avaible for that");
+      } else {
+        for (var i = 0; i < info.length; i++) {
+          $('#doctorInfo').append(`<li><h3>${info[i].profile.first_name} ${info[i].profile.last_name} ${info[i].profile.title}</h3><li>`);
+         $('#doctorInfo').append(`<li>Address: ${info[i].practices[0].visit_address.street} ${info[i].practices[0].visit_address.city} ${info[i].practices[0].visit_address.state} ${info[i].practices[0].visit_address.zip}</li>`);
+         $('#doctorInfo').append(`<li>Phone: ${info[i].practices[0].phones[0].number} </li>`);
+        }
+      }
+
     });
   });
 });
--- src/doctorLookup.js
+++ src/doctorLookup.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 
 export default class userSearch{
+  
  getName(searchName) {
    let url = "https://api.betterdoctor.com/2016-03-01/doctors?name=$" + searchName + "&location=or-portland&user_key=" + process.env.exports.apiKey;
 return new Promise(function(resolve,reject){
